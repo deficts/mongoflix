e8c631a90fe5fd39493bbe2c9d71dbae6a509032
deficts <diegoar7@live.com>
2020-05-28 01:40:20 -0500

    Pagina de detalle, mostrando imagen de poster

--- src/app/movie-detail/movie-detail.page.ts
+++ src/app/movie-detail/movie-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-movie-detail',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.page.scss'],
 })
 export class MovieDetailPage implements OnInit {
+  information=null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private movieService:MovieService) { 
+
+  }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.movieService.getInfo(id).subscribe(res=>{
+      console.log(res[0]);
+      this.information=res[0];
+    })
   }
 
 }
