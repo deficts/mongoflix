1130098aac5a1c22f96376dc719ff486e8f0fb98
deficts <diegoar7@live.com>
2020-05-29 19:34:15 -0500

    Cambiando enum por hash

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -3,24 +3,24 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
-export enum SearchType{
-  all='All',
-  action='Action',
-  adventure='Adventure',
-  animation='Animation',
-  comedy='Comedy',
-  crime='Crime',
-  documentary='Documentary',
-  drama='Drama',
-  family='Family',
-  fantasy='Fantasy',
-  horror='Horror',
-  scifi='Sci-Fi',
-  short='Short',
-  romance='Romance',
-  sport='Sport',
-  thriller='Thriller',
-  war='War'
+export const SearchType = {
+  'All':'',
+  'Action':'Action',
+  'Adventure':'Adventure',
+  'Animation':'Animation',
+  'Comedy':'Comedy',
+  'Crime':'Crime',
+  'Documentary':'Documentary',
+  'Drama':'Drama',
+  'Family':'Family',
+  'Fantasy':'Fantasy',
+  'Horror':'Horror',
+  'Scifi':'Sci-Fi',
+  'Short':'Short',
+  'Romance':'Romance',
+  'Sport':'Sport',
+  'Thriller':'Thriller',
+  'War':'War'
 }
 
 @Injectable({
@@ -32,15 +32,45 @@ export class MovieService {
   constructor(private http: HttpClient) { }
   searchValues=Object.keys(SearchType);
 
-  searchData(title:string, type:SearchType): Observable<any>{
-    return this.http.get(`${this.url}?title=${(title)}&genre=${type}`).pipe(
-      map(results=>{
-        return results;
-      })
-    );
+  searchData(title:string, type): Observable<any>{
+    if(title){
+      return this.http.get(`${this.url}?title=${(title)}&genre=${type}`).pipe(
+        map(results=>{
+          return results;
+        })
+      );
+    }
   }
 
   getInfo(id){
     return this.http.get(`${this.url}?id=${id}`);
   }
+
+  editData(obj){
+    return this.http.put(`${this.url}?title=${obj.title}&score=${obj.score}&year=${obj.year}
+    &genre=${obj.genre}&id=${obj.id}&link=${obj.link}&poster=${obj.poster}`,null).pipe(
+      map(results=>{
+        return results;
+      })
+    )
+  }
+
+  createObject(obj){
+    return this.http.post(`${this.url}?title=${obj.title}&score=${obj.score}&year=${obj.year}
+    &genre=${obj.genre}&id=${obj.id}&link=${obj.link}&poster=${obj.poster}`,null).pipe(
+      map(results=>{
+        return results;
+      })
+    )
+  }
+
+  deleteObject(id){
+    console.log("deleting");
+    console.log(`${this.url}?id=${id}`);
+    this.http.delete(`${this.url}?id=${id}`).subscribe(res=>{
+      return res;
+    })
+
+  }
+
 }
