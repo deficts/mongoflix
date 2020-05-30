29b1c9a5ba13a3e7544180691f274dcb58626969
Diego <diegoar7@live.com>
2020-05-30 17:20:09 -0500

    Arreglando select y refresh al entrar a la page

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -11,7 +11,7 @@ export class MoviesPage implements OnInit {
   results: Observable<any>;
   searchTerm: string = '';
   searchType = SearchType;
-  selectedType;
+  selectedType = 'All';
   searchValues=this.movieService.searchValues;
   constructor(private movieService: MovieService) { }
 
@@ -21,6 +21,14 @@ export class MoviesPage implements OnInit {
     console.log("error");
     console.log(i)
     i.poster='./assets/default.png'
+  }
+
+  ionViewWillEnter(){
+    console.log("borrado")
+    if(this.results){
+      console.log("refresh")
+      this.results = this.movieService.searchData(this.searchTerm,this.searchType[this.selectedType])
+    }
   }
 
   onSearchChanged(){
